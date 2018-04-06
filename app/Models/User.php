0baedb1a3219c1d5a89ec15fe936441d1161529c
vagrant <vagrant@homestead>
2018-04-06 00:25:35 +0000

    Add user statuses

--- app/Models/User.php
+++ app/Models/User.php
@@ -36,6 +36,11 @@ class User extends Authenticatable
         });
     }
 
+    public function statuses()
+    {
+        return $this->hasMany(Status::class);
+    }
+
     public function gravatar($size = '100')
     {
         $hash = md5(strtolower(trim($this->attributes['email'])));
@@ -45,5 +50,11 @@ class User extends Authenticatable
     public function sendPasswordResetNotification($token)
     {
         $this->notify(new ResetPassword($token));
+    }
+
+    public function feed()
+    {
+        return $this->statuses()
+            ->orderBy('created_at', 'desc');
     }
 }
